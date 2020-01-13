f220f1607437a67142c458e25e6ad8543df3b248
brunokiafuka <brunokiafuka@live.com.pt>
2020-01-13 21:47:42 +0200

    bug fix

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Deployed with Travis CI 🚀
+          Deployed with Travis 🚀
         </a>
       </header>
     </div>
